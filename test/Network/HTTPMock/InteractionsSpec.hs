a34e333436da79a981492b78dbbf6018a9f5ec75
Michael Xavier <michael@michaelxavier.net>
2013-03-12 13:14:32 -0700

    First set of useful tests

--- test/Network/HTTPMock/InteractionsSpec.hs
+++ test/Network/HTTPMock/InteractionsSpec.hs
@@ -5,6 +5,9 @@ module Network.HTTPMock.InteractionsSpec (spec) where
 import ClassyPrelude
 import Control.Lens
 import Data.Default
+import Network.HTTP.Types (http11)
+import Network.Wai (RequestBodyLength(KnownLength))
+import Network.Socket (SockAddr(SockAddrUnix))
 import Test.Hspec
 
 import Network.HTTPMock.Types
@@ -14,9 +17,34 @@ spec :: Spec
 spec = do
   describe "getResponse" $ do
     describe "no matches" $ do
+      let mocker = def
       it "returns Nothing" $
-        getResponse req def ^. _1 `shouldBe` Nothing
+        getResponse req mocker ^. _1 `shouldBe` Nothing
       it "returns an unmodified Mocker" $
-        getResponse req def ^. _2 `shouldBe` def
+        getResponse req mocker ^. _2 `shouldBe` mocker
 
-  where req = undefined
+    describe "with AlwaysReturn match" $ do
+      let matcher = RequestMatcher (\reqToCheck -> "/foo" == rawPathInfo reqToCheck)
+      let mocker = def & responder %~ fakedResponses <>~ [(matcher, AlwaysReturns "yep")]
+      it "returns the stubbed response" $
+        getResponse req mocker ^. _1 `shouldBe` Just "yep"
+      it "returns an unmodified Mocker" $
+        getResponse req mocker ^. _2 `shouldBe` mocker
+
+req :: Request
+req = Request {
+    requestMethod     = "GET"
+  , httpVersion       = http11
+  , rawPathInfo       = "/foo"
+  , rawQueryString    = "?wat=true"
+  , serverName        = "example.com"
+  , serverPort        = 4568
+  , requestHeaders    = []
+  , isSecure          = False
+  , remoteHost        = SockAddrUnix "doesntmatter"
+  , pathInfo          = ["foo"]
+  , queryString       = [("wat", Just "true")]
+  , requestBody       = undefined
+  , vault             = undefined
+  , requestBodyLength = KnownLength 0
+}
